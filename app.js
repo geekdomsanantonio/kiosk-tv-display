f9395d5be1ac78433d70dbf242fbed8b800f5e3e
Jake Tripp <shmogens@gmail.com>
2017-12-01 19:05:08 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// change number of mins to whatever you want
+
+// currently set at 1 min (60 seconds)
+var minsBeforePageRefresh = 1;
+
+$(document).ready(function() {
+	setInterval(function() {
+		window.location.reload(true);
+	}, minsBeforePageRefresh * 60 * 1000);
+});
